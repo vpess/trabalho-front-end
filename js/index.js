a9d63b4d3bed8a40b76de0d025db802f048d1269
Vinicius Pessanha <vpessanha@indracompany.com>
2021-09-30 21:40:30 -0300

    Commit inicial.

---
+++ js/index.js
@@ -0,0 +1,7 @@
+//menu para mobile
+const burgerIcon = document.querySelector('#burger');
+const navbarMenu = document.querySelector('#nav-links');
+
+burgerIcon.addEventListener('click', () => {
+    navbarMenu.classList.toggle('is-active');
+});
